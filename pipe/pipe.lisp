35899ddc205855bcd5a8cfed31b7043b6d9cef81
kchen29 <kchen29@stuy.edu>
2018-02-07 17:40:03 -0500

    more functions

--- pipe/pipe.lisp
+++ pipe/pipe.lisp
@@ -1,24 +1,32 @@
 (require :sb-posix)
 
+;;alien is a char array
+;;write the string to stdout
+(defun print-alien-string (alien)
+  (format t "~a~%" (alien-to-string alien)))
+
+;;makes string from alien char array
+;;iterate through until an eof is reached
+(defun alien-to-string (alien)
+  (do* ((str (make-array 0 :element-type 'character :fill-pointer 0 :adjustable t))
+        (i 0 (1+ i))
+        (code (deref alien i) (deref alien i))
+        (char (code-char code) (code-char code)))
+       ((zerop code) str)
+    (vector-push-extend char str)))
+
 (defun child-task (in out)
   (sb-posix:close out)
-  (let ((buf (make-alien (array char 1))))
-    (sb-posix:read in buf 20)
-    (do* ((ar (deref buf 0))
-          (i 0 (1+ i))
-          (code (deref ar i) (deref ar i)))
-         ((zerop code))
-      (format t "~a" (code-char code)))
-    (format t "~%")
-    (sb-posix:close in)
-    (free-alien buf)))
+  (with-alien ((buf (array char 20)))
+    (sb-posix:read in (addr buf) 20)
+    (print-alien-string buf)
+    (sb-posix:close in)))
 
 (defun parent-task (in out)
   (sb-posix:close in)
   (let* ((message "this is a message")
          (message-size (1+ (length message)))
          (buf (make-alien-string message)))
-    ;;(print buf)
     (sb-posix:write out buf message-size)
     (sb-posix:close out)
     (free-alien buf)))
